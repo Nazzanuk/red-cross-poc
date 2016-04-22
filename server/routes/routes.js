ec08b40c051b9fe4b3177d01aa27596b374a8cd2
Nathan <nazzanuk@gmail.com>
2016-04-22 17:54:13 +0100

    :)

---
+++ server/routes/routes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Get = require('./get');
+
+module.exports = {
+    init: (app) => {
+        app.get('/print/:url', Get.print);
+        app.get('/print', Get.print);
+        app.get('*', Get.index);
+    }
+};
